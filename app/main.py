fca99bc0085e039c5064e2bed4632ce190be348a
serbina <serbina@net.chalmers.se>
2024-06-10 17:54:36 +0200

    Fix exit function

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
     # Wait for user input
     while True:
         user_input=input()
-        if user_input="exit 0":
+        if user_input=="exit 0":
             break
         print(f"{user_input}: command not found")
         sys.stdout.write("$ ")
