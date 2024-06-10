4dc8683d3bac81f19c45fc189a44cc8a281793e9
serbina <serbina@net.chalmers.se>
2024-06-10 18:01:54 +0200

    Implement type

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtins = ["echo", "type", "exit"]
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
 
@@ -16,6 +17,11 @@ def main():
             break
         if "echo" in user_input:
             print(user_input[5:])
+        if user_input[0:3]="type":
+            if user_input[4:] in builtins:
+                print(f"{user_input[4:]} is a shell builtin")
+            else:
+                print(f"{user_input[4:]} not found")
         else:
             print(f"{user_input}: command not found")
         sys.stdout.write("$ ")
